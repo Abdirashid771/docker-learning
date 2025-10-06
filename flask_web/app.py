3958c64b7116a48b40968d41e9b193d92e364685
Abdirashid771 <abdirashidabubakar19@gmail.com>
2025-10-06 22:14:15 +0100

    Update app.py with Redis counter route

--- flask_web/app.py
+++ flask_web/app.py
@@ -2,15 +2,18 @@
 
 from flask import Flask
 import redis
+
 app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello_world():
-    return f'Welcome to Flask With Redis'
+    return 'Welcome to Flask With Redis'
+
+@app.route('/count')
+def count():
+    count = r.incr('visits')  
+    return f'This page has been visited {count} times.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
-
-
-
-
